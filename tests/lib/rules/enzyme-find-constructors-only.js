225697c473942c07a592359bd47c32a62c83084b
Evan Lloyd <elloyd@patientslikeme.com>
2018-03-05 11:35:08 -0500

    Add examples of correct enzyme.find code

--- tests/lib/rules/enzyme-find-constructors-only.js
+++ tests/lib/rules/enzyme-find-constructors-only.js
@@ -10,7 +10,7 @@
 
 var rule = require("../../../lib/rules/enzyme-find-constructors-only"),
 
-    RuleTester = require("eslint").RuleTester;
+  RuleTester = require("eslint").RuleTester;
 
 
 //------------------------------------------------------------------------------
@@ -21,19 +21,29 @@ const parserOptions = { ecmaVersion: 6 };
 const ruleTester = new RuleTester();
 ruleTester.run("enzyme-find-constructors-only", rule, {
 
-    valid: [
+  valid: [
+    {
+      code: 'const child = wrapper.find(Link)',
+      parserOptions,
+    },
+    {
+      code: 'let child = wrapper.find(Link)',
+      parserOptions,
+    },
+    {
+      code: "let child = wrapper.find('Connect(Link)')",
+      parserOptions,
+    },
+  ],
 
-        // give me some code that won't trigger a warning
-    ],
-
-    invalid: [
-        {
-            code: "const child = wrapper.find('Link');",
-            parserOptions,
-            errors: [{
-                message: "Please use a constructor with `find`, not a string literal",
-                type: "CallExpression"
-            }]
-        }
-    ]
+  invalid: [
+    {
+      code: "const child = wrapper.find('Link');",
+      parserOptions,
+      errors: [{
+        message: "Please use a constructor with `find`, not a string literal",
+        type: "CallExpression"
+      }]
+    }
+  ]
 });
