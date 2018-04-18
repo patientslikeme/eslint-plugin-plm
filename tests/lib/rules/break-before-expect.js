8dd2965e3f69bd80dba12639c86a5b68bc993d03
Evan Lloyd <elloyd@patientslikeme.com>
2018-04-18 10:30:37 -0400

    add break-before-expect rule

--- tests/lib/rules/break-before-expect.js
+++ tests/lib/rules/break-before-expect.js
@@ -10,28 +10,64 @@
 
 var rule = require("../../../lib/rules/break-before-expect"),
 
-    RuleTester = require("eslint").RuleTester;
+  RuleTester = require("eslint").RuleTester;
 
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
+const parserOptions = { ecmaVersion: 6 };
 var ruleTester = new RuleTester();
 ruleTester.run("break-before-expect", rule, {
 
-    valid: [
+  valid: [
+    {
+      code: `
+        const link = wrapper.find('Link');
+        link.instance().onClick();
 
-        // give me some code that won't trigger a warning
-    ],
+        expect(spy).toHaveBeenCalled();
+      `,
+      parserOptions,
+    },
+    {
+      code: `
+        const link = wrapper.find('Link');
+        link.instance().onClick();
 
-    invalid: [
-        {
-            code: "",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
+        expect(spy).toHaveBeenCalled();
+        expect(spy).toBeASpy();
+      `,
+      parserOptions,
+    },
+  ],
+
+  invalid: [
+    {
+      code: `
+        const link = wrapper.find('Link');
+        link.instance().onClick();
+        expect(spy).toHaveBeenCalled();
+      `,
+      parserOptions,
+      errors: [{
+        message: "Put a newline before your first assertion",
+        type: 'CallExpression',
+      }],
+    },
+    {
+      code: `
+        const link = wrapper.find('Link');
+        link.instance().onClick();
+        expect(spy).toHaveBeenCalled();
+        expect(spy).toBeASpy();
+      `,
+      parserOptions,
+      errors: [{
+        message: "Put a newline before your first assertion",
+        type: 'CallExpression',
+      }],
+    },
+  ]
 });
